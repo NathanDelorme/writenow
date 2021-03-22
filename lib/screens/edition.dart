73ad2077b0e0366147a09de2884a4dd1d450b8bc
Nathan Delorme <nathandelorme2001@gmail.com>
2021-03-22 10:34:53 +0100

    Add edition.dart UI file
    Will be used for contain "edition" UI. This UI will be useful for modifying notes.

---
+++ lib/screens/edition.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:adaptive_theme/adaptive_theme.dart';
+
+class Screen extends StatefulWidget {
+  @override
+  EditionScreen createState() => EditionScreen();
+}
+
+class EditionScreen extends State<Screen> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
+
